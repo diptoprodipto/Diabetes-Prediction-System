9299c0e057363370de0759ac7ae0438ad0772ecb
diptoprodipto <diptoprodipto@gmail.com>
2022-04-09 11:42:15 +0600

    optimized predictionAPI.js

--- templates/predictionAPI.js
+++ templates/predictionAPI.js
@@ -1,44 +1,28 @@
-
 function onClickedPredict() {
-    console.log("Predict Button is clicked");
-    console.log("Prediction API is called!");
-    var age = document.getElementById("age");
-    var gender = document.getElementById("gender");
-    var polyuria = document.getElementById("polyuria");
-    var polydipsia = document.getElementById("polydipsia");
-    var sudden = document.getElementById("sudden");
-    var itching = document.getElementById("itching");
-    var irritability = document.getElementById("irritability");
-    var delayed = document.getElementById("delayed");
-    var partial = document.getElementById("partial");
-    var alopecia = document.getElementById("alopecia");
-  
-    var url = "https://diabetespredictionflask.herokuapp.com/predict"; 
-    
-    $.get(url, {
-      age: parseFloat(age.value),
-      gender: parseFloat(gender.value),
-      polyuria: parseFloat(polyuria.value),
-      polydipsia: parseFloat(polydipsia.value),
-      sudden: parseFloat(sudden.value),
-      itching: parseFloat(itching.value),
-      irritability: parseFloat(irritability.value),
-      delayed: parseFloat(delayed.value),
-      partial: parseFloat(partial.value),
-      alopecia: parseFloat(alopecia.value)
-    },function(data, status) {
-        console.log(data.label);
-        if(data.label == 0)
-        {
-          prediction.innerHTML = "<h3>You don't have Diabetes.<br>Keep living healthy. ☺</h3>";
-        }
-        else
-        {
-          prediction.innerHTML = "<h3>You have Diabetes.<br>Consult with a doctor.</h3>"
-        }		  
-        
-        console.log(status);
-    });
-  }
-  
-  
+
+  const url = "https://diabetespredictionflask.herokuapp.com/predict";
+  console.log(url)
+
+  $.get(url, {
+    age: parseFloat($('#age').val()),
+    gender: parseFloat($('#gender').val()),
+    polyuria: parseFloat($('#polyuria').val()),
+    polydipsia: parseFloat($('#polydipsia').val()),
+    sudden: parseFloat($('#sudden').val()),
+    itching: parseFloat($('#itching').val()),
+    irritability: parseFloat($('#irritability').val()),
+    delayed: parseFloat($('#delayed').val()),
+    partial: parseFloat($('#partial').val()),
+    alopecia: parseFloat($('#alopecia').val())
+  }, function (response, status) {
+    if (response.label === 0) {
+      prediction.innerHTML = "<h3>You don't have Diabetes.<br>Keep living healthy. ☺</h3>";
+    }
+    else if (response.label === 1) {
+      prediction.innerHTML = "<h3>You have Diabetes.<br>Consult with a doctor.</h3>"
+    }
+    else {
+      prediction.innerHTML = "<h3>Please provide all the information.</h3>"
+    }
+  });
+}
